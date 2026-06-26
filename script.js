30e2086cb96471e58c64fd519c6ea0ac514e5631
commercesaichozzo-dev <commerce.saichozzo@gmail.com>
2026-06-26 15:32:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
-let words=[
-"universita",
-"computer",
-"streaming",
-"telefono",
-"programmazione",
-"javascript",
-"github",
-"overlay",
-"internet",
-"monitor",
-"microfono"
-];
+// Esempio di variabili (adatta le tue se hanno nomi diversi)
+let parolaSegreta = "JAVASCRIPT"; 
+let lettereRivelate = Array(parolaSegreta.length).fill("_"); // [ '_', '_', '_', ... ]
+let timerSuggerimento = null;
 
-let currentWord="";
-let display=[];
-
-function newWord(){
-
-currentWord=words[Math.floor(Math.random()*words.length)];
-
-display=currentWord.split("").map(()=>"_");
-
-update();
-
+// Funzione che mostra la grafica a schermo (quella con i trattini che si vede in image_e3ab89.png)
+function aggiornaGraficaSito() {
+    // Sostituisci 'contenitore-parola' con l'ID del tuo elemento HTML reale
+    document.getElementById("contenitore-parola").innerText = lettereRivelate.join(" ");
 }
 
-function update(){
+// Funzione principale che rivela una lettera casuale ancora nascosta
+function rivelaLetteraCasuale() {
+    let indiciNascosti = [];
 
-document.getElementById("word").innerHTML=display.join(" ");
+    // Trova le posizioni di tutte le lettere che sono ancora dei trattini "_"
+    for (let i = 0; i < lettereRivelate.length; i++) {
+        if (lettereRivelate[i] === "_") {
+            indiciNascosti.push(i);
+        }
+    }
 
+    // Se rimangono lettere nascoste (lasciamo l'ultima coperta per non regalare la vittoria!)
+    if (indiciNascosti.length > 1) {
+        // Scegli un indice a caso tra quelli nascosti
+        let indiceCasuale = indiciNascosti[Math.floor(Math.random() * indiciNascosti.length)];
+        
+        // Riveliamo la lettera corretta in quella posizione
+        lettereRivelate[indiceCasuale] = parolaSegreta[indiceCasuale];
+        
+        // Aggiorna la grafica del gioco su OBS
+        aggiornaGraficaSito();
+        console.log("Suggerimento automatico: rivelata una lettera!");
+    } else {
+        // Se la parola è quasi tutta rivelata, ferma il timer
+        clearInterval(timerSuggerimento);
+    }
 }
 
-window.revealLetter=function(letter){
+// Funzione da attivare quando inizia una nuova partita/parola
+function avviaNuovaPartita() {
+    // ... resetta la parola, i trattini, ecc. ...
+    
+    // Cancella eventuali timer precedenti per sicurezza
+    if (timerSuggerimento) clearInterval(timerSuggerimento);
 
-for(let i=0;i<currentWord.length;i++){
-
-if(currentWord[i]==letter){
-
-display[i]=letter;
-
+    // Fai partire il loop: esegue 'rivelaLetteraCasuale' ogni 20000 millisecondi (20 secondi)
+    timerSuggerimento = setInterval(rivelaLetteraCasuale, 20000);
 }
 
+// Quando la parola viene indovinata dalla chat di Twitch, ricordati di resettare il timer!
+function parolaIndovinata() {
+    clearInterval(timerSuggerimento);
+    alert("La chat ha indovinato!");
 }
-
-update();
-
-if(display.join("")==currentWord){
-
-document.getElementById("winner").innerHTML="Parola indovinata!";
-
-setTimeout(()=>{
-
-document.getElementById("winner").innerHTML="";
-
-newWord();
-
-},5000);
-
-}
-
-}
-
-newWord();
